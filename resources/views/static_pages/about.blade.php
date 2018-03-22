38e4e05c93fdfa85d7bd31c11413466273d969b3
PangJieyu <PangJieyu@github.com>
2018-03-22 20:19:29 +0800

    git add -a

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+/**
+ * Created by PhpStorm.
+ * User: PangJieyu
+ * Date: 2018/3/22
+ * Time: 19:08
+ */
+<html>
+    <head>
+        <title>Sample App</title>
+    </head>
+    <body>
+        <h1>ABOUT</h1>
+    </body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+/**
+ * Created by PhpStorm.
+ * User: PangJieyu
+ * Date: 2018/3/22
+ * Time: 19:07
+ */
+<html>
+    <head>
+        <title>Sample App</title>
+    </head>
+    <body>
+        <h1>HOME</h1>
+    </body>
+</html>
