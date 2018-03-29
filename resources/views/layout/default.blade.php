850a5c6dca3677684383d04a4ca8698ae713525c
PangJieyu <pang.jie.yu@sohu.com>
2018-03-29 12:45:02 +0000

    Finish some pages

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,9 +1,17 @@
-<!DOTYPE>
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>@yield('title','sample')</title>
-    </head>
-    <body>
+<head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+@include('layout._header')
+
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
         @yield('content')
-    </body>
+        @include('layout._footer')
+    </div>
+</div>
+</body>
 </html>
