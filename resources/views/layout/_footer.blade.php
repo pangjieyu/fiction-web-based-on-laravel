4efdaa9171c404ea20f49e5ed9ed863da1101f92
PangJieyu <PangJieyu@github.com>
2018-03-30 10:40:08 +0800

    用户注册页面

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -3,12 +3,12 @@
         <small class="slogon">
             <img class="brand-icon" src = "https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
             <a href="https://www.baidu.com">
-                百度
+                百度一下
             </a>
         </small>
         <nav>
             <ul>
-                <li><a href="/about">关于</a> </li>
+                <li><a href="{{route('about')}}">关于</a> </li>
             </ul>
         </nav>
     </footer>
