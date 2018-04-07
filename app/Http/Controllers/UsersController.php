7b58aab24320d17da4a53158597b5e8e311a829d
PangJieyu <PangJieyu@github.com>
2018-04-07 21:08:22 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,7 +25,7 @@ class UsersController extends Controller
     public function create()
     {
         //
-        return view('user.create');
+        return view('users.create');
     }
 
     /**
@@ -37,6 +37,18 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success','欢迎');
+        return redirect()->route('users.show', [$user]);
     }
 
     /**
