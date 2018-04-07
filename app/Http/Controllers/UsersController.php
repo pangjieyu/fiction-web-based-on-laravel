c52654b81828c4c1604eca56aadceb3390f3c558
PangJieyu <PangJieyu@github.com>
2018-04-07 21:45:32 +0800

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -47,6 +48,7 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+        Auth::login($user);
         session()->flash('success','欢迎');
         return redirect()->route('users.show', [$user]);
     }
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -14,5 +14,7 @@
         @include('layout._footer')
     </div>
 </div>
+
+<script src="/js/app.js"></script>
 </body>
 </html>
